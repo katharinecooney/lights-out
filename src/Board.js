f2f5601ae6ac188f1386a1c55a58c83469da3b8a
Katharine Cooney <katharinecooney@gmail.com>
2019-06-30 11:42:35 -0500

    Add styling to Board-title

--- src/Board.js
+++ src/Board.js
@@ -69,11 +69,18 @@ class Board extends Component {
       tableBoard.push(<tr key={y}>{row}</tr>)
     }
     return (
-      <table className="Board">
-        <tbody>
-          {tableBoard}
-        </tbody>
-      </table>
+      <div>
+        <div className="Board-title">
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
+        </div>
+        <table className="Board">
+          <tbody>
+            {tableBoard}
+          </tbody>
+        </table>
+      </div>
+      
     );
   }
 }
